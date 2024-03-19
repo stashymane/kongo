8cac6b4e43b2575f2ffed79059e83d0661d64b54
stashymane <aestheticrice@gmail.com>
2024-03-19 16:51:26 +0200

    Move filters to builder class

--- src/main/kotlin/dev/stashy/mongoservices/MongoService.kt
+++ src/main/kotlin/dev/stashy/mongoservices/MongoService.kt
@@ -1,10 +1,9 @@
 package dev.stashy.mongoservices
 
-import com.mongodb.client.model.Filters
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
+import dev.stashy.mongoservices.builders.FilterBuilder
 import org.bson.conversions.Bson
 import kotlin.reflect.KClass
-import kotlin.reflect.KProperty1
 
 /**
  * Class for defining a MongoDB-based storage service, with various utility functions to ease in writing queries.
@@ -36,21 +35,5 @@ abstract class MongoService<T : Any>(
      */
     val collection = database.getCollection(name, type.java)
 
-    infix fun <V> KProperty1<T, V>.equals(value: V): Bson = Filters.eq(name, value)
-    infix fun <V> KProperty1<T, V>.notEquals(value: V): Bson = Filters.ne(name, value)
-
-    infix fun <V> KProperty1<T, V>.greaterThan(value: V & Any): Bson = Filters.gt(name, value)
-    infix fun <V> KProperty1<T, V>.lessThan(value: V & Any): Bson = Filters.lt(name, value)
-
-    infix fun <V> KProperty1<T, V>.greaterThanOrEquals(value: V & Any): Bson = Filters.gte(name, value)
-    infix fun <V> KProperty1<T, V>.lessThanOrEquals(value: V & Any): Bson = Filters.lte(name, value)
-
-    infix fun <V> KProperty1<T, V>.containedIn(value: V): Bson = Filters.`in`(name, value)
-    infix fun <V> KProperty1<T, V>.notContainedIn(value: V): Bson = Filters.nin(name, value)
-
-    infix fun <V> KProperty1<T, V>.all(value: V): Bson = Filters.all(name, value)
-
-    infix fun Bson.and(value: Bson): Bson = Filters.and(this, value)
-    infix fun Bson.or(value: Bson): Bson = Filters.or(this, value)
-    infix fun Bson.nor(value: Bson): Bson = Filters.nor(this, value)
+    inline fun filter(fn: FilterBuilder<T>.() -> Bson): Bson = fn(FilterBuilder())
 }
---
+++ src/main/kotlin/dev/stashy/mongoservices/builders/FilterBuilder.kt
@@ -0,0 +1,25 @@
+package dev.stashy.mongoservices.builders
+
+import com.mongodb.client.model.Filters
+import org.bson.conversions.Bson
+import kotlin.reflect.KProperty1
+
+class FilterBuilder<T> {
+    infix fun <V> KProperty1<T, V>.equals(value: V): Bson = Filters.eq(name, value)
+    infix fun <V> KProperty1<T, V>.notEquals(value: V): Bson = Filters.ne(name, value)
+
+    infix fun <V> KProperty1<T, V>.greaterThan(value: V & Any): Bson = Filters.gt(name, value)
+    infix fun <V> KProperty1<T, V>.lessThan(value: V & Any): Bson = Filters.lt(name, value)
+
+    infix fun <V> KProperty1<T, V>.greaterThanOrEquals(value: V & Any): Bson = Filters.gte(name, value)
+    infix fun <V> KProperty1<T, V>.lessThanOrEquals(value: V & Any): Bson = Filters.lte(name, value)
+
+    infix fun <V> KProperty1<T, V>.containedIn(value: V): Bson = Filters.`in`(name, value)
+    infix fun <V> KProperty1<T, V>.notContainedIn(value: V): Bson = Filters.nin(name, value)
+
+    infix fun <V> KProperty1<T, V>.all(value: V): Bson = Filters.all(name, value)
+
+    infix fun Bson.and(value: Bson): Bson = Filters.and(this, value)
+    infix fun Bson.or(value: Bson): Bson = Filters.or(this, value)
+    infix fun Bson.nor(value: Bson): Bson = Filters.nor(this, value)
+}
