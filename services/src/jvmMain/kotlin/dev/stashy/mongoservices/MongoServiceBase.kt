8d1862740e3dd90325b79076f07b69fddaefb07e
stashymane <aestheticrice@gmail.com>
2024-06-14 18:44:10 +0300

    Add links to equivalent builders from mongodb

--- services/src/jvmMain/kotlin/dev/stashy/mongoservices/MongoServiceBase.kt
+++ services/src/jvmMain/kotlin/dev/stashy/mongoservices/MongoServiceBase.kt
@@ -9,12 +9,33 @@ interface MongoServiceBase<T : Any> {
     suspend fun init()
 }
 
+
+/**
+ * Builder for sorts.
+ * @see com.mongodb.client.model.Sorts
+ */
 inline fun <T : Any> MongoServiceBase<T>.sort(fn: SortBuilder<T>.() -> Bson): Bson = fn(SortBuilder())
 
+/**
+ * Builder for indexes.
+ * @see com.mongodb.client.model.Indexes
+ */
 inline fun <T : Any> MongoServiceBase<T>.index(fn: IndexBuilder<T>.() -> Bson): Bson = fn(IndexBuilder())
 
+/**
+ * Builder for updates.
+ * @see com.mongodb.client.model.Updates
+ */
 inline fun <T : Any> MongoServiceBase<T>.update(fn: UpdateBuilder<T>.() -> Bson): Bson = fn(UpdateBuilder())
 
+/**
+ * Builder for filters.
+ * @see com.mongodb.client.model.Filters
+ */
 inline fun <T : Any> MongoServiceBase<T>.filter(fn: FilterBuilder<T>.() -> Bson): Bson = fn(FilterBuilder())
 
+/**
+ * Builder for projections.
+ * @see com.mongodb.client.model.Projections
+ */
 inline fun <T : Any> MongoServiceBase<T>.projection(fn: ProjectionBuilder<T>.() -> Bson): Bson = fn(ProjectionBuilder())
