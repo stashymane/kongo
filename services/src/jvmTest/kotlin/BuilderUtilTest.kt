a61f4fe332b6a17ad10643907233a8d1db19a341
stashymane <aestheticrice@gmail.com>
2024-12-31 03:55:53 +0200

    add testing serialname fetching

---
+++ services/src/jvmTest/kotlin/BuilderUtilTest.kt
@@ -0,0 +1,19 @@
+import dev.stashy.mongoservices.builders.serialName
+import kotlinx.serialization.SerialName
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class BuilderUtilTest {
+    class TestClass(
+        @SerialName("StringProperty")
+        val prop1: String,
+        @SerialName("IntProperty")
+        val prop2: Int
+    )
+
+    @Test
+    fun testSerialName() {
+        assertEquals("StringProperty", TestClass::prop1.serialName())
+        assertEquals("IntProperty", TestClass::prop2.serialName())
+    }
+}
