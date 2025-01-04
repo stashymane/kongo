806d855706150dfc28926ac1231a0b667b6a7504
stashymane <aestheticrice@gmail.com>
2025-01-05 01:08:05 +0200

    more codecregistry overrides

--- services/src/jvmMain/kotlin/dev/stashy/mongoservices/DatabaseExtensions.kt
+++ services/src/jvmMain/kotlin/dev/stashy/mongoservices/DatabaseExtensions.kt
@@ -1,8 +1,16 @@
 package dev.stashy.mongoservices
 
+import com.mongodb.kotlin.client.coroutine.MongoClient
+import com.mongodb.kotlin.client.coroutine.MongoCollection
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
 import dev.stashy.mongoservices.builders.RegistryBuilder
 import dev.stashy.mongoservices.builders.codecRegistry
 
 fun MongoDatabase.withCodecRegistry(fn: RegistryBuilder.() -> Unit) =
     withCodecRegistry(codecRegistry(fn))
+
+fun <T : Any> MongoCollection<T>.withCodecRegistry(fn: RegistryBuilder.() -> Unit) =
+    withCodecRegistry(RegistryBuilder().apply(fn).build())
+
+fun MongoClient.withCodecRegistry(fn: RegistryBuilder.() -> Unit) =
+    withCodecRegistry(RegistryBuilder().apply(fn).build())
