0f6314dc09f603cb21d33f133129d77e44db6c1c
stashymane <aestheticrice@gmail.com>
2025-05-14 01:00:48 +0300

    remove redundant builder functions

--- services/src/jvmMain/kotlin/dev/stashy/kongo/service/KongoService.kt
+++ services/src/jvmMain/kotlin/dev/stashy/kongo/service/KongoService.kt
@@ -3,9 +3,7 @@ package dev.stashy.kongo.service
 import com.mongodb.client.model.CreateCollectionOptions
 import com.mongodb.kotlin.client.coroutine.MongoCollection
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
-import dev.stashy.kongo.builders.*
 import dev.stashy.kongo.collection.CollectionProvider
-import org.bson.conversions.Bson
 import kotlin.properties.ReadOnlyProperty
 
 /**
@@ -68,33 +66,3 @@ inline fun <reified T : Any> KongoService<T>.meta(
 ): ReadOnlyProperty<Any?, KongoService.Info<T>> = ReadOnlyProperty { thisRef, property ->
     KongoService.Info(name, T::class.java, options)
 }
-
-/**
- * Builder for sorts.
- * @see com.mongodb.client.model.Sorts
- */
-inline fun <T : Any> KongoService<T>.sort(fn: SortBuilder<T>.() -> Bson): Bson = fn(SortBuilder())
-
-/**
- * Builder for indexes.
- * @see com.mongodb.client.model.Indexes
- */
-inline fun <T : Any> KongoService<T>.index(fn: IndexBuilder<T>.() -> Bson): Bson = fn(IndexBuilder())
-
-/**
- * Builder for updates.
- * @see com.mongodb.client.model.Updates
- */
-inline fun <T : Any> KongoService<T>.update(fn: UpdateBuilder<T>.() -> Bson): Bson = fn(UpdateBuilder())
-
-/**
- * Builder for filters.
- * @see com.mongodb.client.model.Filters
- */
-inline fun <T : Any> KongoService<T>.filter(fn: FilterBuilder<T>.() -> Bson): Bson = fn(FilterBuilder())
-
-/**
- * Builder for projections.
- * @see com.mongodb.client.model.Projections
- */
-inline fun <T : Any> KongoService<T>.projection(fn: ProjectionBuilder<T>.() -> Bson): Bson = fn(ProjectionBuilder())
