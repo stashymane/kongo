f4b0a817126ec4be40b46ee064433030b553e19e
stashymane <aestheticrice@gmail.com>
2025-05-14 12:26:55 +0300

    add db result helper extensions

---
+++ services/src/jvmMain/kotlin/dev/stashy/kongo/ResultExtensions.kt
@@ -0,0 +1,23 @@
+package dev.stashy.kongo
+
+import com.mongodb.client.result.InsertManyResult
+import com.mongodb.client.result.InsertOneResult
+import com.mongodb.client.result.UpdateResult
+import org.bson.BsonObjectId
+import org.bson.BsonValue
+
+/**
+ * @see [InsertOneResult.getInsertedId]
+ */
+val InsertOneResult.documentId: DocumentId? get() = insertedId?.asObjectId()?.toDocumentId()
+
+/**
+ * @see [InsertManyResult.getInsertedIds]
+ */
+val InsertManyResult.documentId: List<DocumentId>
+    get() = insertedIds.values.map(BsonValue::asObjectId).map(BsonObjectId::toDocumentId)
+
+/**
+ * @see [UpdateResult.getUpsertedId]
+ */
+val UpdateResult.upsertedDocumentId: DocumentId? get() = upsertedId?.asObjectId()?.toDocumentId()
