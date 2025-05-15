1e9950a869002ded180c8237e7ba19c0b1ad366f
stashymane <aestheticrice@gmail.com>
2025-05-15 21:53:17 +0300

    add return value to transaction

--- services/src/jvmMain/kotlin/dev/stashy/kongo/SessionExtensions.kt
+++ services/src/jvmMain/kotlin/dev/stashy/kongo/SessionExtensions.kt
@@ -3,16 +3,24 @@ package dev.stashy.kongo
 import com.mongodb.TransactionOptions
 import com.mongodb.kotlin.client.coroutine.ClientSession
 
+/**
+ * Automatically starts a transaction, commits if no exceptions are thrown and aborts the transaction otherwise.
+ *
+ * @see [ClientSession.startTransaction]
+ * @see [ClientSession.commitTransaction]
+ * @see [ClientSession.abortTransaction]
+ */
 suspend inline fun <T> ClientSession.transaction(
     options: TransactionOptions.Builder.() -> Unit = {},
     fn: suspend (session: ClientSession) -> T
-) {
+): Result<T> {
     startTransaction(TransactionOptions.builder().apply(options).build())
     try {
         val value = fn.invoke(this)
         commitTransaction()
+        return Result.success(value)
     } catch (e: Exception) {
         abortTransaction()
-        throw e
+        return Result.failure(e)
     }
 }
