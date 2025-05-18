dbc912590682db07c975e589f208b57b206ab746
stashymane <aestheticrice@gmail.com>
2025-05-18 13:06:21 +0300

    add test case for BSON documentid serialization

--- services/src/jvmTest/kotlin/DocumentIdSerializationTest.kt
+++ services/src/jvmTest/kotlin/DocumentIdSerializationTest.kt
@@ -1,7 +1,13 @@
 import dev.stashy.kongo.DocumentId
+import dev.stashy.kongo.toDocumentId
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
+import org.bson.BsonDocument
+import org.bson.BsonDocumentWriter
+import org.bson.codecs.EncoderContext
+import org.bson.codecs.kotlinx.KotlinSerializerCodec
+import org.bson.types.ObjectId
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -18,10 +24,28 @@ class DocumentIdSerializationTest {
     }
 
     @Test
-    fun `contextual documentId inside an object`() {
+    fun `documentId inside an object`() {
         val documentId = DocumentId("test")
         val testObj = TestObj(documentId, "test")
 
         assertEquals("{\"_id\":\"test\",\"foo\":\"test\"}", Json.encodeToString(testObj))
+    }
+
+    @Test
+    fun `documentId bson serialization`() {
+        val original = ObjectId()
+        val documentId = original.toDocumentId()
+
+        val codec = KotlinSerializerCodec.create<DocumentId>()!!
+        val doc = BsonDocument()
+        val writer = BsonDocumentWriter(doc)
+        val context = EncoderContext.builder().build()
+
+        writer.writeStartDocument()
+        writer.writeName("_id")
+        codec.encode(writer, documentId, context)
+        writer.writeEndDocument()
+
+        assertEquals(original, doc.getObjectId("_id").value)
     }
 }
